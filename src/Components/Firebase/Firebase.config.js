5007b60fc9e0fd73edfe0b57fd66d3df09f67786
rabiul-idb <rabiul.idb40@gmail.com>
2024-12-08 09:26:37 +0600

    env variable is done for firebase

--- src/Components/Firebase/Firebase.config.js
+++ src/Components/Firebase/Firebase.config.js
@@ -7,13 +7,21 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBopBdTFEqlhOs9GZ8E2ShiqsOrhy3R54c",
-  authDomain: "sportsgear-66d62.firebaseapp.com",
-  projectId: "sportsgear-66d62",
-  storageBucket: "sportsgear-66d62.firebasestorage.app",
-  messagingSenderId: "724277399674",
-  appId: "1:724277399674:web:b9e0406e05d8e23740abc3"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBopBdTFEqlhOs9GZ8E2ShiqsOrhy3R54c",
+//   authDomain: "sportsgear-66d62.firebaseapp.com",
+//   projectId: "sportsgear-66d62",
+//   storageBucket: "sportsgear-66d62.firebasestorage.app",
+//   messagingSenderId: "724277399674",
+//   appId: "1:724277399674:web:b9e0406e05d8e23740abc3"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
